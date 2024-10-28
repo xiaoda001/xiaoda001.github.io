7c0971ce2ffb58dd3691df79a1beda4315a63e4c
traveler-小达 <1010161292@qq.com>
2024-10-28 23:32:32 +0800

    fix: 替换logo，增加上传到github page脚本

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+cd ./docs/.vitepress/dist
+
+git init
+
+git add .
+
+git commit -m "重新部署"
+
+git remote add origin git@github.com:xiaoda001/xiaoda001.github.io.git
+
+git push -f origin master
