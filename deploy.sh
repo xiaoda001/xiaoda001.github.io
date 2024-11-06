99b981a6b9453d8f53826f35bd13a2897552abf0
traveler-小达 <1010161292@qq.com>
2024-11-07 01:38:48 +0800

    fix: sh文件修改

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 cd ./docs/.vitepress/dist
 
-git init
+# git init
 
 git add .
 
 git commit -m "重新部署"
 
-git remote add origin git@github.com:xiaoda001/xiaoda001.github.io.git
+# git remote add origin git@github.com:xiaoda001/xiaoda001.github.io.git
 
 git push -f origin master
