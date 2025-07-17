70215df075732108de24d3ec162efe8daa4d7cef
= <1010161292@qq.com>
2025-07-18 01:11:29 +0800

    fix: 增加脚本

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 !/bin/bash
-
-cd ./docs/.vitepress/dist
-
 # git init
 
 git add .
 
-git commit -m "重新部署"
+git commit -m "修改"
 
 # git remote add blog git@github.com:xiaoda001/xiaoda001.github.io.git
 
